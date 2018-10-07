c481d9d6ae165e93ae61b04a68d04ef9bd75bcb5
Dipankar <developer.dipankar@gmail.com>
2018-10-08 00:07:37 +0530

    facebook auth, log, global v1.1

--- config/db.js
+++ config/db.js
@@ -1,4 +1,5 @@
 // MongoDB Connection
+mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect(env.MONGO_URL, { useNewUrlParser: true });
 mongoose.set('useCreateIndex', true);
